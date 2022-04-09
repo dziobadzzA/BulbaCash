5e7055d980852778f2f22f2e80fd4e01552e7017
Bahdan Dzioba <dziobadzz@gmail.com>
2022-04-10 00:28:19 +0300

    feat: add BulbaCashApplication

--- app/src/main/java/com/service/bulbacash/presentation/MainActivity.kt
+++ app/src/main/java/com/service/bulbacash/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.service.bulbacash
+package com.service.bulbacash.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.service.bulbacash.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
