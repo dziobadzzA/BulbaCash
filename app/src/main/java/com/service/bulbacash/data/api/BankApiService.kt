5ee58dfff576cb5d53b269ea0d0cc27bfb8c16b2
Bahdan Dzioba <dziobadzz@gmail.com>
2022-04-10 01:25:00 +0300

    feat: add CurrencyPojo

--- app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
+++ app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
@@ -1,4 +1,12 @@
 package com.service.bulbacash.data.api
 
+import com.service.bulbacash.data.api.pojo.CurrencyPojo
+import retrofit2.http.GET
+
 interface BankApiService {
+    /**
+     * Get all currency
+     */
+    @GET("exrates/currencies")
+    suspend fun getCurrency(): List<CurrencyPojo?>
 }
