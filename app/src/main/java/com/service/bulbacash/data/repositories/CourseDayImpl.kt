fc364057aaf46772493636b04e56aee1d2e0d04c
Bogdan Dzioba <dziobadzz@gmail.com>
2022-04-10 17:05:56 +0300

    refactor: CourseDayRepository

--- app/src/main/java/com/service/bulbacash/data/repositories/CourseDayImpl.kt
+++ app/src/main/java/com/service/bulbacash/data/repositories/CourseDayImpl.kt
@@ -1,4 +1,22 @@
 package com.service.bulbacash.data.repositories
 
-class CourseDayImpl {
+import com.service.bulbacash.data.api.BankApiService
+import com.service.bulbacash.data.api.pojo.RatePojo
+import com.service.bulbacash.domain.models.Rate
+import com.service.bulbacash.domain.repositories.CourseDayRepository
+import javax.inject.Inject
+
+class CourseDayImpl @Inject constructor(
+    private val bankApi: BankApiService,
+    private val mapRatePojo: RatePojo
+):CourseDayRepository{
+
+    override suspend fun getRateToday(id: Int): Rate {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRateDate(id: Int, onDate: String): Rate {
+        TODO("Not yet implemented")
+    }
+
 }
