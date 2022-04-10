63f1e786f7e90e16f0e8e88fb3450f850dad31d3
Bahdan Dzioba <dziobadzz@gmail.com>
2022-04-10 11:52:49 +0300

    feat: add rates methods to Api Service

--- app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
+++ app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
@@ -1,8 +1,10 @@
 package com.service.bulbacash.data.api
 
 import com.service.bulbacash.data.api.pojo.CurrencyPojo
+import com.service.bulbacash.data.api.pojo.RatePojo
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BankApiService {
 
@@ -13,8 +15,44 @@ interface BankApiService {
     suspend fun getCurrency(): List<CurrencyPojo?>
 
     /**
-     * Get all current currency
+     * Get current currency
+     *  @param id Cur_ID
      */
     @GET("exrates/currencies/{id}")
     suspend fun getCurrentCurrency(@Path("id") id:Int): CurrencyPojo?
+
+    /**
+     * Get all rates today
+     */
+    @GET("exrates/rates?periodicity=0")
+    suspend fun getRatesToday(): List<RatePojo?>
+
+    /**
+     * Get all rates to date
+     *  @param ondate Date request
+     *  @param periodicity 0 - day, 1 - month
+     */
+    @GET("exrates/rates")
+    suspend fun getRatesDate(
+        @Query("ondate") onDate:String,
+        @Query("periodicity") periodiCity: Int
+    ): List<RatePojo?>
+
+    /**
+     * Get rate today
+     *  @param id Cur_ID
+     */
+    @GET("exrates/rates/{id}")
+    suspend fun getRateToday(@Path("id") id:Int): RatePojo?
+
+    /**
+     * Get rate today
+     *  @param id Cur_ID
+     *  @param ondate Date request
+     */
+    @GET("exrates/rates/{id}")
+    suspend fun getRateDate(@Path("id") id:Int,
+                            @Query("ondate") onDate:String
+    ): RatePojo?
+
 }
