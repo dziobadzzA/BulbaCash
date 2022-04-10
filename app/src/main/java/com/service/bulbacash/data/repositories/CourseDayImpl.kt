764cfbb1f1b2cae88e803b23893071ae8bee69db
Bogdan Dzioba <dziobadzz@gmail.com>
2022-04-10 17:13:07 +0300

    feat: override method getRateToday

--- app/src/main/java/com/service/bulbacash/data/repositories/CourseDayImpl.kt
+++ app/src/main/java/com/service/bulbacash/data/repositories/CourseDayImpl.kt
@@ -1,21 +1,21 @@
 package com.service.bulbacash.data.repositories
 
 import com.service.bulbacash.data.api.BankApiService
-import com.service.bulbacash.data.api.pojo.RatePojo
+import com.service.bulbacash.data.mappers.RatePojoToRate
 import com.service.bulbacash.domain.models.Rate
 import com.service.bulbacash.domain.repositories.CourseDayRepository
 import javax.inject.Inject
 
 class CourseDayImpl @Inject constructor(
     private val bankApi: BankApiService,
-    private val mapRatePojo: RatePojo
+    private val mapRatePojoToRate: RatePojoToRate
 ):CourseDayRepository{
 
-    override suspend fun getRateToday(id: Int): Rate {
-        TODO("Not yet implemented")
+    override suspend fun getRateToday(id: Int): Rate? {
+        return bankApi.getRateToday(id)?.let { mapRatePojoToRate.map(it) }
     }
 
-    override suspend fun getRateDate(id: Int, onDate: String): Rate {
+    override suspend fun getRateDate(id: Int, onDate: String): Rate? {
         TODO("Not yet implemented")
     }
 
