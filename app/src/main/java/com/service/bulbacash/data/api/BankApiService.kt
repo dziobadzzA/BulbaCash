265a10058b55c5d7dee6be0e21a8c9e67e72a172
Bahdan Dzioba <dziobadzz@gmail.com>
2022-04-10 11:12:54 +0300

    feat: add getCurrentCurrency to Api Service

--- app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
+++ app/src/main/java/com/service/bulbacash/data/api/BankApiService.kt
@@ -2,6 +2,7 @@ package com.service.bulbacash.data.api
 
 import com.service.bulbacash.data.api.pojo.CurrencyPojo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BankApiService {
 
@@ -10,4 +11,10 @@ interface BankApiService {
      */
     @GET("exrates/currencies")
     suspend fun getCurrency(): List<CurrencyPojo?>
+
+    /**
+     * Get all current currency
+     */
+    @GET("exrates/currencies/{id}")
+    suspend fun getCurrentCurrency(@Path("id") id:Int): CurrencyPojo?
 }
