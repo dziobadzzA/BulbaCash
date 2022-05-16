3194d941c96e68e33db1da20a3d6fdc9fb818469
Bogdan Dzioba <dziobadzz@gmail.com>
2022-05-16 23:48:28 +0300

    fix: add RateEntityToRate

--- app/src/main/java/com/service/bulbacash/data/mappers/RateMapper.kt
+++ app/src/main/java/com/service/bulbacash/data/mappers/RateMapper.kt
@@ -1,10 +1,13 @@
 package com.service.bulbacash.data.mappers
 
+import androidx.room.ColumnInfo
 import com.service.bulbacash.data.api.pojo.RatePojo
 import com.service.bulbacash.data.api.pojo.RateShortPojo
+import com.service.bulbacash.data.db.entity.RateEntity
 import com.service.bulbacash.domain.models.Rate
 import com.service.bulbacash.domain.models.RateShort
 import com.service.bulbacash.domain.utill.Mapper
+import org.joda.time.DateTime
 
 class RatePojoToRate : Mapper<RatePojo, Rate> {
     override fun map(from: RatePojo): Rate {
@@ -24,6 +27,19 @@ class RateShortPojoToRateShort : Mapper<RateShortPojo, RateShort> {
         return RateShort(
             Cur_ID = from.Cur_ID,
             Date = from.Date,
+            Cur_OfficialRate = from.Cur_OfficialRate
+        )
+    }
+}
+
+class RateEntityToRate: Mapper<RateEntity, Rate> {
+    override fun map(from: RateEntity): Rate {
+        return Rate(
+            Date = DateTime.now().toString(),
+            Cur_ID = from.Cur_ID,
+            Cur_Abbreviation = from.Cur_Abbreviation,
+            Cur_Scale = from.Cur_Scale,
+            Cur_Name = from.Cur_Name,
             Cur_OfficialRate = from.Cur_OfficialRate
         )
     }
