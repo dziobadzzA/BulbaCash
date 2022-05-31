eb5d2506e92b19022c3b69d9578c1c482ee919a5
Bogdan Dzioba <dziobadzz@gmail.com>
2022-05-31 23:28:35 +0300

    feat: add BulbaCashDatabase

--- app/src/main/java/com/service/bulbacash/presentation/ui/buckets/WorkBucketsViewModel.kt
+++ app/src/main/java/com/service/bulbacash/presentation/ui/buckets/WorkBucketsViewModel.kt
@@ -1,9 +1,12 @@
 package com.service.bulbacash.presentation.ui.buckets
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.service.bulbacash.data.mappers.CurrencyEntityToCurrency
 import com.service.bulbacash.domain.models.Currency
 import com.service.bulbacash.domain.usecases.GetAllBucketsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,7 +14,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class WorkBucketsViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val getAllBucketsUseCase: GetAllBucketsUseCase,
+    private val mapCurrencyEntityToCurrency: CurrencyEntityToCurrency
 ):ViewModel() {
 
     var list: List<Currency> = listOf()
