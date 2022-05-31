ac36d53602e88bfa43b1ee48e1caf38fd8c87c32
Bogdan Dzioba <dziobadzz@gmail.com>
2022-06-01 00:26:42 +0300

    feat: add items to spinners

--- app/src/main/java/com/service/bulbacash/presentation/ui/buckets/WorkBucketsFragment.kt
+++ app/src/main/java/com/service/bulbacash/presentation/ui/buckets/WorkBucketsFragment.kt
@@ -2,11 +2,15 @@ package com.service.bulbacash.presentation.ui.buckets
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.service.bulbacash.R
 import com.service.bulbacash.databinding.WorkBucketsFragmentBinding
+import com.service.bulbacash.domain.models.Currency
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class WorkBucketsFragment: Fragment(R.layout.work_buckets_fragment){
@@ -20,7 +24,15 @@ class WorkBucketsFragment: Fragment(R.layout.work_buckets_fragment){
 
 
         binding?.apply {
+            btnAddBucket.setOnClickListener {
 
+            }
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.list.collect {
+                addItemsToList(list = it)
+            }
         }
 
         viewModel.getCurrency()
@@ -29,6 +41,17 @@ class WorkBucketsFragment: Fragment(R.layout.work_buckets_fragment){
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun addItemsToList(list: List<Currency>) {
+        binding?.apply {
+            val adapter = ArrayAdapter(requireContext().applicationContext,
+                android.R.layout.simple_spinner_item, list)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerFirst.adapter = adapter
+            spinnerSecond.adapter = adapter
+            // imageFirst.setImageResource(R.drawable.ad)
+        }
     }
 
 }
