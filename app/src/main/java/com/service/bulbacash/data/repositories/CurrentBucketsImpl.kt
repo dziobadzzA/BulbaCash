cc87e78f81404de9c33f79c97b0c89c6d4324feb
Bogdan Dzioba <dziobadzz@gmail.com>
2022-06-12 17:51:22 +0300

    feat: add bulbaCashDAO CurrentBucketsImpl

--- app/src/main/java/com/service/bulbacash/data/repositories/CurrentBucketsImpl.kt
+++ app/src/main/java/com/service/bulbacash/data/repositories/CurrentBucketsImpl.kt
@@ -1,10 +1,14 @@
 package com.service.bulbacash.data.repositories
 
+import com.service.bulbacash.data.db.dao.BulbaCashDAO
 import com.service.bulbacash.domain.models.BucketRate
 import com.service.bulbacash.domain.repositories.CurrentBucketsRepository
 import javax.inject.Inject
 
-class CurrentBucketsImpl @Inject constructor(): CurrentBucketsRepository {
+class CurrentBucketsImpl @Inject constructor(
+    private val bulbaCashDAO: BulbaCashDAO
+    ): CurrentBucketsRepository {
+
     override suspend fun addBucket(bucketRate: BucketRate): Boolean {
         TODO("Not yet implemented")
     }
@@ -16,4 +20,5 @@ class CurrentBucketsImpl @Inject constructor(): CurrentBucketsRepository {
     override suspend fun deleteBucket(bucketRate: BucketRate): Boolean {
         TODO("Not yet implemented")
     }
+
 }
