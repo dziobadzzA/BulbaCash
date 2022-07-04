fd5d6a8140991017fc809238816304e63007d344
Bogdan Dzioba <dziobadzz@gmail.com>
2022-07-04 22:15:31 +0300

    feat: add table StencilEntity and refactor fields in model ItemStencilEntity

--- app/src/main/java/com/service/bulbacash/data/db/entity/StencilEntity.kt
+++ app/src/main/java/com/service/bulbacash/data/db/entity/StencilEntity.kt
@@ -1,19 +1,17 @@
 package com.service.bulbacash.data.db.entity
 
+
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "item_stencil")
-data class ItemStencilEntity(
+@Entity(tableName = "stencil")
+data class StencilEntity(
 
     @PrimaryKey(autoGenerate = true)
     var id : Long = 0,
 
-    @ColumnInfo(name = "Cur_ID")
-    val Cur_ID: Int,
-
-    @ColumnInfo(name = "Cost_name")
-    val CostName: String
+    @ColumnInfo(name="name_stencil")
+    val nameStencil: String
 
 )
