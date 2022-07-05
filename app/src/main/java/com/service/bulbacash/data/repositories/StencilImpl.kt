eb55b8014ff5faa0a3e29332ae19ea6c0b01a82e
Bogdan Dzioba <dziobadzz@gmail.com>
2022-07-05 21:47:00 +0300

    feat: add StencilImpl

---
+++ app/src/main/java/com/service/bulbacash/data/repositories/StencilImpl.kt
@@ -0,0 +1,19 @@
+package com.service.bulbacash.data.repositories
+
+import com.service.bulbacash.data.db.dao.BulbaCashDAO
+import com.service.bulbacash.domain.models.Stencil
+import com.service.bulbacash.domain.repositories.StencilRepository
+import javax.inject.Inject
+
+class StencilImpl @Inject constructor(
+    private val bulbaCashDAO: BulbaCashDAO):StencilRepository
+{
+    override suspend fun saveStencil(stencil: Stencil) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteStencil(stencil: Stencil) {
+        TODO("Not yet implemented")
+    }
+
+}
