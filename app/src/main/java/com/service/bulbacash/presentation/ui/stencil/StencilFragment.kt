3674bfd71a17df66aa0a67d9ccfbecdedf098d17
Bogdan Dzioba <dziobadzz@gmail.com>
2022-07-05 22:18:38 +0300

    feat: add navigation from StencilFragment to ItemStencilFragment

--- app/src/main/java/com/service/bulbacash/presentation/ui/stencil/StencilFragment.kt
+++ app/src/main/java/com/service/bulbacash/presentation/ui/stencil/StencilFragment.kt
@@ -3,8 +3,10 @@ package com.service.bulbacash.presentation.ui.stencil
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.service.bulbacash.R
 import com.service.bulbacash.databinding.StencilFragmentBinding
+import com.service.bulbacash.presentation.ui.course.CourseFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +19,13 @@ class StencilFragment: Fragment(R.layout.stencil_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = StencilFragmentBinding.bind(view)
+        binding?.apply {
+            floatingActionButton.setOnClickListener {
+                findNavController().navigate(
+                    StencilFragmentDirections.actionNavTemplatesToItemStencilFragment()
+                )
+            }
+        }
 
     }
 
