3fec8f5a5121a39a2ea945307381cd7dc130ffe2
Bogdan Dzioba <dziobadzz@gmail.com>
2022-07-05 22:11:45 +0300

    feat: add empty ItemStencilFragment

---
+++ app/src/main/java/com/service/bulbacash/presentation/ui/stencil/item/ItemStencilFragment.kt
@@ -0,0 +1,28 @@
+package com.service.bulbacash.presentation.ui.stencil.item
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.service.bulbacash.R
+import com.service.bulbacash.databinding.ItemsStencilLayoutBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ItemStencilFragment: Fragment(R.layout.items_stencil_layout) {
+
+    private var binding: ItemsStencilLayoutBinding? = null
+    // private val viewModel:  by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = ItemsStencilLayoutBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+}
