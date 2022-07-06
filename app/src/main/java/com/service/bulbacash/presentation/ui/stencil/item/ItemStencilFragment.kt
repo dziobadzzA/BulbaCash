b138f45ff228cdc5564a5573492f0934e435c4b7
Bogdan Dzioba <dziobadzz@gmail.com>
2022-07-06 22:42:12 +0300

    feat: add spinner to Stencil

--- app/src/main/java/com/service/bulbacash/presentation/ui/stencil/item/ItemStencilFragment.kt
+++ app/src/main/java/com/service/bulbacash/presentation/ui/stencil/item/ItemStencilFragment.kt
@@ -2,8 +2,10 @@ package com.service.bulbacash.presentation.ui.stencil.item
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.service.bulbacash.R
 import com.service.bulbacash.databinding.ItemsStencilLayoutBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,14 +22,37 @@ class ItemStencilFragment: Fragment(R.layout.items_stencil_layout) {
         binding = ItemsStencilLayoutBinding.bind(view)
 
         binding?.apply {
-            // valEd.onItemSelectedListener =
+            valEd.onItemSelectedListener = viewModel.mapAdapterCountries
+                .getListenerToSpinner(imageView = imageIcon)
         }
 
+        lifecycleScope.launchWhenCreated {
+            viewModel.list.collect {
+                if (it.isNotEmpty()) {
+                    addItemsToList(
+                        list = viewModel.mapAdapterCountries.getListText(),
+                        startImage = viewModel.mapAdapterCountries.getListWithIcon(it[0])
+                    )
+                }
+            }
+        }
+
+        viewModel.getListForSpinner()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun addItemsToList(list: List<String>, startImage: Int) {
+        binding?.apply {
+            val adapter = ArrayAdapter(requireContext().applicationContext,
+                android.R.layout.simple_spinner_item, list)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            valEd.adapter = adapter
+            imageIcon.setImageResource(startImage)
+        }
     }
 
 }
--- app/src/main/java/com/service/bulbacash/presentation/ui/stencil/item/ItemStencilViewModel.kt
+++ app/src/main/java/com/service/bulbacash/presentation/ui/stencil/item/ItemStencilViewModel.kt
@@ -5,20 +5,31 @@ import com.service.bulbacash.di.AdapterCountries
 import com.service.bulbacash.domain.models.Currency
 import com.service.bulbacash.domain.usecases.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class ItemStencilViewModel @Inject constructor(
    /* private val saveStencilUseCase: SaveStencilUseCase,
-    private val deleteStencilUseCase: DeleteStencilUseCase,
-    val mapAdapterWorkBuckets: AdapterCountries*/
+    private val deleteStencilUseCase: DeleteStencilUseCase,*/
+    private val getAllBucketsUseCase: GetAllBucketsUseCase,
+    val mapAdapterCountries: AdapterCountries
+
 ): ViewModel() {
 
     private var _list = MutableStateFlow(listOf<Currency>())
     val list = _list.asStateFlow()
 
+    fun getListForSpinner() {
+        CoroutineScope(Dispatchers.IO).launch {
+            _list.value = getAllBucketsUseCase.invoke()
+            mapAdapterCountries.setListAdapter(list.value)
+        }
+    }
 
 }
